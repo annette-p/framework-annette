0bddd2613612b88bfa3b3c776cd8872552483660
Annette Poh <coding.net.p@gmail.com>
2021-10-25 10:22:34 +0000

    create poster database table

---
+++ routes/landing.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const router = express.Router();  // #1 - create a new Express router object, add a new new route to express Router
+
+router.get('/', (req,res)=>{
+    res.render('landing/index')
+})
+
+router.get('/about-us', (req, res) => {
+    res.render('landing/about-us')
+})
+
+router.get('/contact-us', (req,res)=>{
+    res.render('landing/contact-us')
+})
+
+module.exports = router; 
+
